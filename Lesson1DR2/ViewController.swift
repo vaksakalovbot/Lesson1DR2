f36eb3b4fd66eb3186990c68846ce56c6e15d2da
Vladimir <vaksakalovbot@yandex.ru>
2020-05-19 17:45:50 +0300

    Initial Commit

---
+++ Lesson1DR2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson1DR2
+//
+//  Created by Vladimir on 19/05/2020.
+//  Copyright © 2020 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
